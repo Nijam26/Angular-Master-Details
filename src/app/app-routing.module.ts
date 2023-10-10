9e0814901a4d90603c3dd1d86b0e20d72998eb14
Nijam26 <nijam93.ctg@gmail.com>
2023-10-10 19:53:06 +0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Country } from './models/country';
+import { CountryListComponent } from './components/country-list/country-list.component';
+import { CityListComponent } from './components/city-list/city-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'city', pathMatch:'full'},
+  { path: 'country', component: CountryListComponent },
+  { path: 'city', component: CityListComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
